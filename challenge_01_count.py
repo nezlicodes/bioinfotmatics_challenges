23275b3dfb51d51d34d01f74065e532ffdb4ed0a
nezlicodes <benmeradi.lilyanezli@gmail.com>
2019-11-20 07:41:52 +0100

    first challenge solved!

---
+++ challenge_01_count.py
@@ -0,0 +1,6 @@
+#Count the number of times a pattern repeats in a DNA seq
+import re;
+def count(text, pattern):
+    return re.subn(pattern, '', text, flags=re.IGNORECASE)[1]
+
+print(count('ATCGATATat', 'AT'))
